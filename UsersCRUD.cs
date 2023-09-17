02ab4c24a1655b68984a90c43a1de4f2604adbc9
Kamryn Plock <kamplock402@gmail.com>
2023-09-17 18:54:50 -0500

    Add project files.

---
+++ UsersCRUD.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using TigerByte;
+
+public class UsersCRUD
+{
+    
+    private IMongoCollection<Users> collection;
+
+    public UsersCRUD(IMongoDatabase database)
+    {
+        // Initialize the collection with the appropriate collection from MongoDB
+        collection = database.GetCollection<Users>("users");
+    }
+
+
+
+    //insert one - create
+    public Users InsertUser(Users collection, Users theuser)
+    {
+        try
+        {
+            collection.InsertOne(theuser);
+            return theuser;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Something went wrong trying to insert the user. Message: {e.Message}");
+            Console.WriteLine(e);
+            return null;
+        }
+    }
+
+    //read all users
+    public void getAllUsers()
+    {
+        var allDocs = collection.Find(Builders<Users>.Filter.Empty).ToList();
+
+        foreach (Users users in allDocs)
+        {
+            Console.WriteLine($"{users.Name}'s email address is {users.Email} and " +
+                $"has an id of {users.UserId}");
+            Console.WriteLine();
+        }
+    }
+
+    //read one user
+    public string getOneUserByName(string username)
+    {
+        var findFilter = Builders<Users>.Filter.Eq("Name", username);
+
+        var findResult = collection.Find(findFilter).FirstOrDefault();
+
+        if (findResult == null)
+        {
+            return "I didn't find a user with the name that matched.";
+        }
+        return findResult.ToString();
+    }
+
+    //update 
+    public string updateDoc(Users theuser, string nameorEmail, string updated)
+    {
+        var findFilter = Builders<Users>.Filter.Eq(nameorEmail, updated);
+
+        if (nameorEmail.Equals("Email"))
+        {
+            var updateFilter = Builders<Users>.Update.Set(t => t.Email, updated);
+
+            // The following FindOneAndUpdateOptions specify that we want the *updated* document
+            // to be returned to us. By default, we get the document as it was *before*
+            // the update.
+
+            var options = new FindOneAndUpdateOptions<Users, Users>()
+            {
+                ReturnDocument = ReturnDocument.After
+            };
+
+            // The updatedDocument object is a Users object that reflects the
+            // changes we just made.
+            var updatedDocument = collection.FindOneAndUpdate(findFilter,
+                updateFilter, options);
+
+            return updatedDocument.ToString();
+        }
+        else if (nameorEmail.Equals("Name"))
+        {
+            var updateFilter = Builders<Users>.Update.Set(t => t.Name, updated);
+
+            // The following FindOneAndUpdateOptions specify that we want the *updated* document
+            // to be returned to us. By default, we get the document as it was *before*
+            // the update.
+
+            var options = new FindOneAndUpdateOptions<Users, Users>()
+            {
+                ReturnDocument = ReturnDocument.After
+            };
+
+            // The updatedDocument object is a Recipe object that reflects the
+            // changes we just made.
+            var updatedDocument = collection.FindOneAndUpdate(findFilter,
+                updateFilter, options);
+
+            return updatedDocument.ToString();
+        }
+        else
+        {
+            return "We couldn't complete your request.";
+        }
+    }
+        //delete 1
+    public void deleteOneUser(string email)
+    {
+        var deleteResult = collection.DeleteOne(Builders<Users>.Filter.Eq("Email", email));
+        Console.WriteLine($"I deleted {deleteResult.DeletedCount} records.");
+    }
+
+        //delete all
+    public void deleteAll()
+    {
+        var filter = Builders<Users>.Filter.Empty;
+        var result = collection.DeleteMany(filter);
+        Console.WriteLine($"I deleted {result.DeletedCount} records.");
+    }
+
+
+    
+}
---
+++ UsersMainTest.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TigerByte
+{
+    class UsersMainTest
+	{
+        public static void Main(string[] args)
+        {
+            //TODO talk to Chris or Mark about security
+            string password = Environment.GetEnvironmentVariable("<password>");
+
+            var mongoUri = "mongodb+srv://root:" + password + "@cluster0.nzth94y.mongodb.net/?retryWrites=true&w=majority";
+
+            // The IMongoClient is the object that defines the connection to our
+            // datastore (Atlas, for example)
+            IMongoClient client;
+
+            // An IMongoCollection defines a connection to a specific MongoDB
+            // collection. Your app may have one or many different IMongoCollection
+            // objects.
+            IMongoCollection<Users> UsersColl;
+
+            //try to connect
+            try
+            {
+                client = new MongoClient(mongoUri);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("There was a problem connecting to your " +
+                    "Atlas cluster. Check that the URI includes a valid " +
+                    "username and password, and that your IP address is " +
+                    $"in the Access List. Message: {e.Message}");
+                Console.WriteLine(e);
+                Console.WriteLine();
+                return;
+            }
+
+            //one database 3 collections (users, problems, submissions)
+            var dbName = "TigerByteDB";
+            var collectionName = "UsersColl";
+
+            var collection = client.GetDatabase(dbName)
+                   .GetCollection<Users>(collectionName);
+
+            
+        }
+        
+
+
+
+
+    }
+
+    public class Users
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int UserId { get; set; }
+
+        public Users(string name, string email, int userid)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.UserId = userid;
+
+        }
+
+        /// <summary>
+        /// This static method is just here so we have a convenient way
+        /// to generate sample user data.
+        /// </summary>
+        /// <returns>A list of Users</returns>       
+        public static List<Users> GetUsers()
+        {
+            return new List<Users>()
+            {
+
+                new Users("Olivia Navarro", "olivia.navarro@doane.edu", 1),
+                new Users("Kamryn Plock", "kamryn.plock@doane.edu", 2),
+                new Users("Mark Meysenburg", "mark.meysenburg@doane.edu", 3),
+                new Users("Alec Engebretson", "alec.engebretson@doane.edu", 4),
+
+
+            };
+        }
+
+        internal void InsertOne(Users theuser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
