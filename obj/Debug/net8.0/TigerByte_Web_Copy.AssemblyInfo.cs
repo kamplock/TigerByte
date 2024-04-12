c4ea67d45ec5c098b63d52a66f6ab80b5b2a590c
Kamryn Plock <kamplock402@gmail.com>
2024-04-11 19:36:19 -0500

    made a few tweaks on filter criteria

--- obj/Debug/net8.0/TigerByte_Web_Copy.AssemblyInfo.cs
+++ obj/Debug/net8.0/TigerByte_Web_Copy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TigerByte_Web_Copy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f21b2ebf69fdf3d404d2a3b5c7cda92e5fd4d620")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ded1bc94862f15c59b63da04470a95627eed1794")]
 [assembly: System.Reflection.AssemblyProductAttribute("TigerByte_Web_Copy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TigerByte_Web_Copy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
